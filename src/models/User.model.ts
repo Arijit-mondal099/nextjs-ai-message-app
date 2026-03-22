c2cc00a7237796e3293138fbe13994a81ef88357
Arijit Mondal <arijitm717@gmail.com>
2026-03-22 11:13:45 +0530

    user sechem done

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,5 +1,5 @@
 
-import { Document, Schema } from "mongoose"
+import { Document, Model, model, models, Schema } from "mongoose"
 
 export interface Message extends Document {
     content: string;
@@ -9,9 +9,10 @@ export interface User extends Document {
     username: string;
     email: string;
     password: string;
+    isVerified: boolean;
     verifyCode: string;
     verifyCodeExpiry: Date;
-    isAppcetingMessage: boolean;
+    isAcceptingMessage: boolean;
     messages: Message[]
 }
 
@@ -33,8 +34,44 @@ const userSchema: Schema<User> = new Schema(
         username: {
             type: String,
             trim: true,
-            required: [true, "Username is required"]
-        }
+            required: [true, "Username is required"],
+            unique: [true, "Username alredy taken"],
+            maxLength: [30, "Username can't be 30 charaters long"]
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: [true, "Email is required"],
+            unique: [true, "Email alredy taken"],
+            lowercase: true
+        },
+        password: {
+            type: String,
+            trim: true,
+            required: [true, "Password is required"]
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        verifyCode: {
+            type: String,
+            required: [true, "Verify code is required"]
+        },
+        verifyCodeExpiry: {
+            type: Date,
+            required: [true, "Verify code expiry is required"]
+        },
+        isAcceptingMessage: {
+            type: Boolean,
+            default: true
+        },
+        messages: [messageSchema]
+    },
+    {
+        timestamps: true
     }
 )
 
+
+export default (models.User as Model<User>) || model<User>("User", userSchema)
